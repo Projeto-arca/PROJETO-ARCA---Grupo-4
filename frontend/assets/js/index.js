ead273fe10c30661636c8203f5c498714472c203
guzazo <gcaliari938@gmail.com>
2026-06-03 09:52:43 -0300

    Ajustando menu de navegação e redirecionamentos do login

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -5,11 +5,11 @@ function logar() {
     if (usuario === 'tutor' && senha === '123456') {
         window.location.href = 'frontend/pages/tutor/dashboard.html';
     } else if (usuario === 'candidato' && senha === 'cand!098') {
-        window.location.href = 'frontend/pages/candidato/dashboard.html';
-    } else if (usuario === 'ong' && senha === 'ong$-135') {
+        window.location.href = 'frontend/pages/tutor/dashboard.html';
+    } else if (usuario === 'Ong' && senha === 'ong$-135') {
         window.location.href = 'frontend/pages/ong/dashboard.html';
     } else if (usuario === 'prefeitura' && senha === 'pref@456') {
-        window.location.href = 'frontend/pages/prefeitura/dashboard.html';
+        window.location.href = 'frontend/pages/ong/dashboard.html';
     } else {
         alert('Usuário ou senha incorretos. Tente novamente.');
     }
